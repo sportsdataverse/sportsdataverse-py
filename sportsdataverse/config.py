d3e38d959be5aaa677a6449a9d6f82af58351eab
armstjc <70788681+armstjc@users.noreply.github.com>
2022-06-30 18:02:54 -0400

    Fixed bug where CFB fast functions don't reference the correct  branch

--- sportsdataverse/config.py
+++ sportsdataverse/config.py
@@ -1,11 +1,11 @@
 SGITHUB = 'https://raw.githubusercontent.com/sportsdataverse/'
 NFLVERSEGITHUB='https://raw.githubusercontent.com/nflverse/'
 
-CFB_BASE_URL = SGITHUB+'cfbfastR-data/master/pbp/parquet/play_by_play_{season}.parquet'
-CFB_ROSTER_URL = SGITHUB+'cfbfastR-data/master/rosters/parquet/rosters_{season}.parquet'
-CFB_TEAM_LOGO_URL  = SGITHUB+'cfbfastR-data/master/teams/teams_colors_logos.parquet'
-CFB_TEAM_SCHEDULE_URL = SGITHUB+'cfbfastR-data/master/schedules/parquet/schedules_{season}.parquet'
-CFB_TEAM_INFO_URL = SGITHUB+'cfbfastR-data/master/team_info/parquet/team_info_{season}.parquet'
+CFB_BASE_URL = SGITHUB+'cfbfastR-data/main/pbp/parquet/play_by_play_{season}.parquet'
+CFB_ROSTER_URL = SGITHUB+'cfbfastR-data/main/rosters/parquet/rosters_{season}.parquet'
+CFB_TEAM_LOGO_URL  = SGITHUB+'cfbfastR-data/main/teams/teams_colors_logos.parquet'
+CFB_TEAM_SCHEDULE_URL = SGITHUB+'cfbfastR-data/main/schedules/parquet/schedules_{season}.parquet'
+CFB_TEAM_INFO_URL = SGITHUB+'cfbfastR-data/main/team_info/parquet/team_info_{season}.parquet'
 
 NFL_BASE_URL = NFLVERSEGITHUB+'nflfastR-data/master/data/play_by_play_{season}.parquet'
 NFL_PLAYER_STATS_URL = NFLVERSEGITHUB+'nflfastR-data/master/data/player_stats.parquet'
---
+++ test.py
@@ -0,0 +1,3 @@
+import sportsdataverse as sdv
+cfb_df = sdv.cfb.cfb_teams()
+print(cfb_df)
