5a4d047debbdf59727aa5f35f12cb3152f6cad5f
saiemgilani <saiem.gilani@gmail.com>
2022-09-28 05:17:17 -0400

    rename nfl to nfl_games

--- sportsdataverse/nfl/__init__.py
+++ sportsdataverse/nfl/__init__.py
@@ -2,4 +2,4 @@ from sportsdataverse.nfl.nfl_loaders import *
 from sportsdataverse.nfl.nfl_pbp import *
 from sportsdataverse.nfl.nfl_schedule import *
 from sportsdataverse.nfl.nfl_teams import *
-from sportsdataverse.nfl.nfl import *
+from sportsdataverse.nfl.nfl_games import *
