00df4d046b6cf83d5508544efc2ed5ff57fc4146
saiemgilani <saiem.gilani@gmail.com>
2023-03-02 22:47:08 -0500

    __init__.py sdv imports

---
+++ sportsdataverse/__init__.py
@@ -0,0 +1,7 @@
+from sportsdataverse.cfb import *
+from sportsdataverse.mbb import *
+from sportsdataverse.nba import *
+from sportsdataverse.nfl import *
+from sportsdataverse.nhl import *
+from sportsdataverse.wbb import *
+from sportsdataverse.wnba import *
