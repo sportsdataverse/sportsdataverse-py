7dac003872639bf1f9d288fec9177d551a884274
saiemgilani <saiem.gilani@gmail.com>
2023-07-14 15:34:43 -0400

    Update wnba package to polars

--- sportsdataverse/wnba/wnba_schedule.py
+++ sportsdataverse/wnba/wnba_schedule.py
@@ -6,7 +6,8 @@ from sportsdataverse.config import WNBA_BASE_URL, WNBA_TEAM_BOX_URL, WNBA_PLAYER
 from sportsdataverse.errors import SeasonNotFoundError
 from sportsdataverse.dl_utils import download, underscore
 
-def espn_wnba_schedule(dates=None, season_type=None, limit = 500) -> pd.DataFrame:
+def espn_wnba_schedule(dates=None, season_type=None, limit = 500,
+                       return_as_pandas = True, **kwargs) -> pd.DataFrame:
     """espn_wnba_schedule - look up the WNBA schedule for a given season
 
     Args:
@@ -17,60 +18,93 @@ def espn_wnba_schedule(dates=None, season_type=None, limit = 500) -> pd.DataFram
     Returns:
         pd.DataFrame: Pandas dataframe containing schedule dates for the requested season.
     """
-    if dates is None:
-        dates = ''
-    else:
-        dates = '&dates=' + str(dates)
-    if season_type is None:
-        season_type = ''
-    else:
-        season_type = '&seasontype=' + str(season_type)
+    url = "http://site.api.espn.com/apis/site/v2/sports/basketball/wnba/scoreboard"
+    params = {
+        'dates': dates,
+        'seasonType': season_type,
+        'limit': limit
+    }
+    resp = download(url=url, params=params, **kwargs)
 
-    url = "http://site.api.espn.com/apis/site/v2/sports/basketball/wnba/scoreboard?limit={}{}{}".format(limit, dates, season_type)
-    resp = download(url=url)
+    ev = pl.DataFrame()
+    events_txt = resp.json()
+    events = events_txt.get('events')
+    if len(events) == 0:
+        return pd.DataFrame() if return_as_pandas else pl.DataFrame()
 
-    ev = pd.DataFrame()
-    if resp is not None:
-        events_txt = json.loads(resp)
-        events = events_txt.get('events')
-        for event in events:
-            event.get('competitions')[0].get('competitors')[0].get('team').pop('links',None)
-            event.get('competitions')[0].get('competitors')[1].get('team').pop('links',None)
-            if event.get('competitions')[0].get('competitors')[0].get('homeAway')=='home':
-                event['competitions'][0]['home'] = event.get('competitions')[0].get('competitors')[0].get('team')
-                event['competitions'][0]['home']['score'] = event.get('competitions')[0].get('competitors')[0].get('score')
-                event['competitions'][0]['home']['winner'] = event.get('competitions')[0].get('competitors')[0].get('winner')
-                event['competitions'][0]['away'] = event.get('competitions')[0].get('competitors')[1].get('team')
-                event['competitions'][0]['away']['score'] = event.get('competitions')[0].get('competitors')[1].get('score')
-                event['competitions'][0]['away']['winner'] = event.get('competitions')[0].get('competitors')[1].get('winner')
-            else:
-                event['competitions'][0]['away'] = event.get('competitions')[0].get('competitors')[0].get('team')
-                event['competitions'][0]['away']['score'] = event.get('competitions')[0].get('competitors')[0].get('score')
-                event['competitions'][0]['away']['winner'] = event.get('competitions')[0].get('competitors')[0].get('winner')
-                event['competitions'][0]['home'] = event.get('competitions')[0].get('competitors')[1].get('team')
-                event['competitions'][0]['home']['score'] = event.get('competitions')[0].get('competitors')[1].get('score')
-                event['competitions'][0]['home']['winner'] = event.get('competitions')[0].get('competitors')[1].get('winner')
+    for event in events:
+        event.get('competitions')[0].get('competitors')[0].get('team').pop('links',None)
+        event.get('competitions')[0].get('competitors')[1].get('team').pop('links',None)
+        if event.get('competitions')[0].get('competitors')[0].get('homeAway') == 'home':
+            event = __extract_home_away(event, 0, 'home')
+            event = __extract_home_away(event, 1, 'away')
+        else:
+            event = __extract_home_away(event, 0, 'away')
+            event = __extract_home_away(event, 1, 'home')
 
-            del_keys = ['broadcasts','geoBroadcasts', 'headlines', 'series', 'situation', 'tickets', 'odds']
-            for k in del_keys:
-                event.get('competitions')[0].pop(k, None)
-            if len(event.get('competitions')[0]['notes'])>0:
-                event.get('competitions')[0]['notes_type'] = event.get('competitions')[0]['notes'][0].get("type")
-                event.get('competitions')[0]['notes_headline'] = event.get('competitions')[0]['notes'][0].get("headline").replace('"','')
-            else:
-                event.get('competitions')[0]['notes_type'] = ''
-                event.get('competitions')[0]['notes_headline'] = ''
-            event.get('competitions')[0].pop('notes', None)
-            x = pd.json_normalize(event.get('competitions')[0], sep='_')
-            x['game_id'] = x['id'].astype(int)
-            x['season'] = event.get('season').get('year')
-            x['season_type'] = event.get('season').get('type')
-            ev = pd.concat([ev,x],axis=0, ignore_index=True)
-    ev = pd.DataFrame(ev)
-    ev.columns = [underscore(c) for c in ev.columns.tolist()]
-    return ev
+        event.get('competitions')[0]['notes_type'] = event.get('competitions')[0]['notes'][0].get("type") if len(event.get('competitions')[0]['notes']) > 0 else ''
+        event.get('competitions')[0]['notes_headline'] = event.get('competitions')[0]['notes'][0].get("headline").replace('"','') if len(event.get('competitions')[0]['notes']) > 0 else ''
+        event.get('competitions')[0]['broadcast_market'] = event.get('competitions')[0].get('broadcasts', [])[0].get('market', "") if len(event.get('competitions')[0].get('broadcasts')) > 0 else ""
+        event.get('competitions')[0]['broadcast_name'] = event.get('competitions')[0].get('broadcasts', [])[0].get('names', [])[0] if len(event.get('competitions')[0].get('broadcasts')) > 0 else ""
+        del_keys = ['geoBroadcasts', 'headlines', 'series', 'situation', 'tickets',
+                    'odds', 'broadcasts', 'notes', 'competitors']
+        for k in del_keys:
+            event.get('competitions')[0].pop(k, None)
 
-def espn_wnba_calendar(season=None, ondays=None) -> pd.DataFrame:
+        x = pl.from_pandas(pd.json_normalize(event.get('competitions')[0], sep = '_'))
+        x = x.with_columns(
+            game_id = (pl.col('id').cast(pl.Int64)),
+            season = (event.get('season').get('year')),
+            season_type = (event.get('season').get('type')),
+            home_linescores = pl.when(pl.col('status_type_description') == 'Postponed')
+                .then(None)
+                .otherwise(pl.col('home_linescores')),
+            away_linescores = pl.when(pl.col('status_type_description') == 'Postponed')
+                .then(None)
+                .otherwise(pl.col('away_linescores')),
+        )
+        x = x[[s.name for s in x if s.null_count() != x.height]]
+        ev = pl.concat([ev, x], how = 'diagonal')
+
+    ev.columns = [underscore(c) for c in ev.columns]
+
+    return ev.to_pandas() if return_as_pandas else ev
+
+def __extract_home_away(event, arg1, arg2):
+    event['competitions'][0][arg2] = (
+        event.get('competitions')[0].get('competitors')[arg1].get('team')
+    )
+    event['competitions'][0][arg2]['score'] = (
+        event.get('competitions')[0].get('competitors')[arg1].get('score')
+    )
+    event['competitions'][0][arg2]['winner'] = (
+        event.get('competitions')[0].get('competitors')[arg1].get('winner')
+    )
+    ## add winner back to main competitors if does not exist
+    event['competitions'][0]['competitors'][arg1]['winner'] = (
+        event.get('competitions')[0].get('competitors')[arg1].get('winner', False)
+    )
+
+    event['competitions'][0][arg2]['linescores'] = (
+        event.get('competitions')[0]
+        .get('competitors')[arg1]
+        .get('linescores', [{'value': 'N/A'}])
+    )
+    ## add linescores back to main competitors if does not exist
+    event['competitions'][0]['competitors'][arg1]['linescores'] = (
+        event.get('competitions')[0]
+        .get('competitors')[arg1]
+        .get('linescores', [])
+    )
+    event['competitions'][0][arg2]['records'] = (
+        event.get('competitions')[0]
+        .get('competitors')[arg1]
+        .get('records', [])
+    )
+    return event
+
+def espn_wnba_calendar(season=None, ondays=None,
+                       return_as_pandas = True, **kwargs) -> pd.DataFrame:
     """espn_wnba_calendar - look up the WNBA calendar for a given season
 
     Args:
@@ -84,39 +118,46 @@ def espn_wnba_calendar(season=None, ondays=None) -> pd.DataFrame:
         ValueError: If `season` is less than 2002.
     """
     if ondays is not None:
-        url = "https://sports.core.api.espn.com/v2/sports/basketball/leagues/wnba/seasons/{}/types/2/calendar/ondays".format(season)
-        resp = download(url=url)
-        txt = json.loads(resp).get('eventDate').get('dates')
-        full_schedule = pd.DataFrame(txt,columns=['dates'])
-        full_schedule['dateURL'] = list(map(lambda x: x[:10].replace("-",""),full_schedule['dates']))
-        full_schedule['url']="http://site.api.espn.com/apis/site/v2/sports/basketball/wnba/scoreboard?dates="
-        full_schedule['url']= full_schedule['url'] + full_schedule['dateURL']
+        full_schedule = __ondays_wnba_calendar(season, **kwargs)
     else:
-        url = "http://site.api.espn.com/apis/site/v2/sports/basketball/wnba/scoreboard?dates={}".format(season)
-        resp = download(url=url)
-        txt = json.loads(resp)['leagues'][0]['calendar']
+        url = f"http://site.api.espn.com/apis/site/v2/sports/basketball/wnba/scoreboard?dates={season}"
+        resp = download(url=url, **kwargs)
+        txt = resp.json().get('leagues')[0].get('calendar')
         datenum = list(map(lambda x: x[:10].replace("-",""),txt))
         date = list(map(lambda x: x[:10],txt))
         year = list(map(lambda x: x[:4],txt))
         month = list(map(lambda x: x[5:7],txt))
         day = list(map(lambda x: x[8:10],txt))
-
-        data = {"season": season,
-                "datetime" : txt,
-                "date" : date,
-                "year": year,
-                "month": month,
-                "day": day,
-                "dateURL": datenum
+        data = {
+            "season": season,
+            "datetime" : txt,
+            "date" : date,
+            "year": year,
+            "month": month,
+            "day": day,
+            "dateURL": datenum
         }
-        full_schedule = pd.DataFrame(data)
-        full_schedule['url']="http://site.api.espn.com/apis/site/v2/sports/basketball/wnba/scoreboard?dates="
-        full_schedule['url']= full_schedule['url'] + full_schedule['dateURL']
-    return full_schedule
+        full_schedule = pl.DataFrame(data)
+        full_schedule = full_schedule.with_columns(
+            url="http://site.api.espn.com/apis/site/v2/sports/basketball/wnba/scoreboard?dates=" +
+                pl.col('dateURL')
+        )
+    return full_schedule.to_pandas() if return_as_pandas else full_schedule
+
+
+def __ondays_wnba_calendar(season, **kwargs):
+    url = f"https://sports.core.api.espn.com/v2/sports/basketball/leagues/wnba/seasons/{season}/types/2/calendar/ondays"
+    resp = download(url=url, **kwargs)
+    txt = resp.json().get('eventDate').get('dates')
+    result = pl.DataFrame(txt, schema=['dates'])
+    result = result.with_columns(dateURL = pl.col('dates').str.slice(0, 10))
+    result = result.with_columns(
+        url="http://site.api.espn.com/apis/site/v2/sports/basketball/wnba/scoreboard?dates=" +
+            pl.col('dateURL')
+    )
+
+    return result
 
 def most_recent_wnba_season():
     today = datetime.date(datetime.now())
-    if today.month >= 5:
-        return today.year
-    else:
-        return today.year - 1
+    return today.year if today.month >= 5 else today.year - 1
--- sportsdataverse/wnba/wnba_teams.py
+++ sportsdataverse/wnba/wnba_teams.py
@@ -1,19 +1,29 @@
 import pandas as pd
+import polars as pl
 import json
 from sportsdataverse.dl_utils import download, underscore
 from urllib.error import URLError, HTTPError, ContentTooShortError
 
-def espn_wnba_teams() -> pd.DataFrame:
+def espn_wnba_teams(return_as_pandas = True, **kwargs) -> pd.DataFrame:
     """espn_wnba_teams - look up WNBA teams
+
+    Args:
+        return_as_pandas (bool): If True, returns a pandas dataframe. If False, returns a polars dataframe.
 
     Returns:
         pd.DataFrame: Pandas dataframe containing teams for the requested league.
+
+    Example:
+        `wnba_df = sportsdataverse.wnba.espn_wnba_teams()`
+
     """
-    ev = pd.DataFrame()
-    url = "http://site.api.espn.com/apis/site/v2/sports/basketball/wnba/teams?limit=1000"
-    resp = download(url=url)
+    url = "http://site.api.espn.com/apis/site/v2/sports/basketball/wnba/teams"
+    params = {
+        "limit": 1000
+    }
+    resp = download(url=url, params = params, **kwargs)
     if resp is not None:
-        events_txt = json.loads(resp)
+        events_txt = resp.json()
 
         teams = events_txt.get('sports')[0].get('leagues')[0].get('teams')
         del_keys = ['record', 'links']
@@ -22,5 +32,5 @@ def espn_wnba_teams() -> pd.DataFrame:
                 team.get('team').pop(k, None)
         teams = pd.json_normalize(teams, sep='_')
     teams.columns = [underscore(c) for c in teams.columns.tolist()]
-    return teams
+    return teams if return_as_pandas else pl.from_pandas(teams)
 
