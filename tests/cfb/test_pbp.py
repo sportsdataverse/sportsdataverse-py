b0a4e1534a27d8666f58c43da96e7067409b3e61
Akshay Easwaran <akeaswaran@me.com>
2025-12-20 10:02:22 -0500

    adding another test case for yardage

--- tests/cfb/test_pbp.py
+++ tests/cfb/test_pbp.py
@@ -572,22 +572,24 @@ def test_yards_per_drive():
 #     assert target_plays.loc[target_plays.index[0], field_name] == expected_yards
 
 @pytest.mark.parametrize(
-    "play_text,yards_field,expected_yards",
+    "game_id,play_text,yards_field,expected_yards",
     [
-        ("(14:46) Shotgun #10 H.King pass complete short right to #1 J.Haynes caught at GT27, for 15 yards to the GT40 (#13 G.Bryant III), 1ST DOWN", "yds_passing", 15),
-        ("(14:46) Shotgun #10 H.King pass complete short right to #1 J.Haynes caught at GT27, for 15 yards to the GT40 (#13 G.Bryant III), 1ST DOWN", "yds_receiving", 15),
-        ("(14:17) No Huddle-Shotgun #10 H.King pass complete short right to #4 I.Canion caught at GT46, for 2 yards to the GT42 fumbled by #4 I.Canion at GT46 forced by #16 C.Peal recovered by SU #8 D.Reese at GT42, End Of Play", "yds_receiving", 2),
-        ("(06:15) Shotgun #10 H.King pass incomplete short left to #17 J.Beetham thrown to SU01", "yds_receiving", 0),
-        ("(13:31) Shotgun #10 H.King rush right for 7 yards gain to the SU30, out of bounds at SU30, 1ST DOWN", "yds_rushed", 7),
-        ("(07:16) No Huddle-Shotgun #1 J.Haynes rush left for 4 yards loss to the SU35 (#6 J.Heard Jr.; #3 K.Singleton)", "yds_rushed", -4),
-        ("(15:00) No Huddle-Shotgun #10 R.Collins pass complete deep right to #2 J.Cook II caught at GT37, for 41 yards to the GT34 (#6 R.Shelley), 1ST DOWN", "yds_passing", 41),
-        ("(15:00) No Huddle-Shotgun #10 R.Collins pass complete deep right to #2 J.Cook II caught at GT37, for 41 yards to the GT34 (#6 R.Shelley), 1ST DOWN", "yds_receiving", 41),
-        ("(09:25) No Huddle-Shotgun #10 R.Collins pass complete short left to #2 J.Cook II caught at SU31, for 4 yards to the SU34 (#2 E.Lightsey)", "yds_passing", 4),
-        ("(05:49) Shotgun #10 H.King pass complete short middle to #85 J.Allen caught at SU33, for 19 yards to the SU09 (#0 B.Long Jr.)", "yds_passing", 19)
+        (401754571, "(14:46) Shotgun #10 H.King pass complete short right to #1 J.Haynes caught at GT27, for 15 yards to the GT40 (#13 G.Bryant III), 1ST DOWN", "yds_passing", 15),
+        (401754571, "(14:46) Shotgun #10 H.King pass complete short right to #1 J.Haynes caught at GT27, for 15 yards to the GT40 (#13 G.Bryant III), 1ST DOWN", "yds_receiving", 15),
+        (401754571, "(14:17) No Huddle-Shotgun #10 H.King pass complete short right to #4 I.Canion caught at GT46, for 2 yards to the GT42 fumbled by #4 I.Canion at GT46 forced by #16 C.Peal recovered by SU #8 D.Reese at GT42, End Of Play", "yds_receiving", 2),
+        (401754571, "(06:15) Shotgun #10 H.King pass incomplete short left to #17 J.Beetham thrown to SU01", "yds_receiving", 0),
+        (401754571, "(13:31) Shotgun #10 H.King rush right for 7 yards gain to the SU30, out of bounds at SU30, 1ST DOWN", "yds_rushed", 7),
+        (401754571, "(07:16) No Huddle-Shotgun #1 J.Haynes rush left for 4 yards loss to the SU35 (#6 J.Heard Jr.; #3 K.Singleton)", "yds_rushed", -4),
+        (401754571, "(15:00) No Huddle-Shotgun #10 R.Collins pass complete deep right to #2 J.Cook II caught at GT37, for 41 yards to the GT34 (#6 R.Shelley), 1ST DOWN", "yds_passing", 41),
+        (401754571, "(15:00) No Huddle-Shotgun #10 R.Collins pass complete deep right to #2 J.Cook II caught at GT37, for 41 yards to the GT34 (#6 R.Shelley), 1ST DOWN", "yds_receiving", 41),
+        (401754571, "(09:25) No Huddle-Shotgun #10 R.Collins pass complete short left to #2 J.Cook II caught at SU31, for 4 yards to the SU34 (#2 E.Lightsey)", "yds_passing", 4),
+        (401754571, "(05:49) Shotgun #10 H.King pass complete short middle to #85 J.Allen caught at SU33, for 19 yards to the SU09 (#0 B.Long Jr.)", "yds_passing", 19),
+        (401777353, "(07:37) Shotgun #10 J.Sayin pass complete short left to #4 J.Smith caught at OSU29, for 5 yards loss to the OSU32 (#12 D.Boykin)", "yds_passing", -5),
+        (401777353, "(07:37) Shotgun #10 J.Sayin pass complete short left to #4 J.Smith caught at OSU29, for 5 yards loss to the OSU32 (#12 D.Boykin)", "yds_receiving", -5)
     ]
 )
-def test_25_yardage_detection(play_text: str, yards_field: str, expected_yards: int):
-    test = CFBPlayProcess(gameId = 401754571)
+def test_25_yardage_detection(game_id: int, play_text: str, yards_field: str, expected_yards: int):
+    test = CFBPlayProcess(gameId = game_id)
     test.espn_cfb_pbp()
     json_dict_stuff = test.run_processing_pipeline()
 
