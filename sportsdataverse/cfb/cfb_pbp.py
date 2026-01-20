437fb15878c7f7b5678d33907181b2d77c0863d4
Akshay Easwaran <akeaswaran@me.com>
2026-01-19 20:59:12 -0500

    fixing punt issues

--- sportsdataverse/cfb/cfb_pbp.py
+++ sportsdataverse/cfb/cfb_pbp.py
@@ -4807,6 +4807,7 @@ class CFBPlayProcess(object):
                 & (play_df.change_of_pos_team == 1),
                 (play_df["kickoff_onside"] == True)
                 & ((play_df["change_of_pos_team"] == True) | (play_df["change_of_poss"] == True)),  # onside recovery
+                (play_df["penalty_flag"] == True & (play_df["start.pos_team.id"] != play_df["end.pos_team.id"]) & ((play_df["change_of_pos_team"] == True) | (play_df["change_of_poss"] == True))),
                 (play_df["penalty_flag"] == True & (play_df["start.pos_team.id"] != play_df["end.pos_team.id"])),
                 (play_df["start.pos_team.id"] != play_df["end.pos_team.id"]),
                 ((play_df["penalty_flag"] == True) & (play_df.penalty_assessed_on_kickoff == True)),
@@ -4823,6 +4824,7 @@ class CFBPlayProcess(object):
                 play_df.lead_wp_before,
                 (1 - play_df.lead_wp_before),
                 (1 - play_df.lead_wp_before),
+                (1 - play_df.wp_after),
                 play_df.wp_after,
                 (1 - play_df.wp_after),
                 WP_start_touchback
@@ -4872,6 +4874,7 @@ class CFBPlayProcess(object):
                 & (play_df.change_of_pos_team == 1),
                 (play_df["kickoff_onside"] == True)
                 & ((play_df["change_of_pos_team"] == True) | (play_df["change_of_poss"] == True)),  # onside recovery
+                (play_df["penalty_flag"] == True & (play_df["start.pos_team.id"] != play_df["end.pos_team.id"]) & ((play_df["change_of_pos_team"] == True) | (play_df["change_of_poss"] == True))),
                 (play_df["penalty_flag"] == True & (play_df["start.pos_team.id"] != play_df["end.pos_team.id"])),
                 (play_df["start.pos_team.id"] != play_df["end.pos_team.id"]),
                 ((play_df["penalty_flag"] == True) & (play_df.penalty_assessed_on_kickoff == True)),
@@ -4890,7 +4893,8 @@ class CFBPlayProcess(object):
                 10,
                 11,
                 12,
-                13
+                13,
+                14
             ],
             default=None,
         )
