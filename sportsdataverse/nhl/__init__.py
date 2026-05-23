72687f46fe8099902572053c9ae246f456e0845b
saiemgilani <saiem.gilani@gmail.com>
2026-05-23 08:09:42 -0400

    feat(nhl): re-export nhl_edge, nhl_stats_rest, nhl_records from package init
    Wire the three new NHL modules into sportsdataverse.nhl so they are
    importable as the rest of the league surface:
    
        from sportsdataverse.nhl import (
            nhl_edge_skaters_skating,
            nhl_stats_rest_season,
            nhl_records_franchise,
        )

--- sportsdataverse/nhl/__init__.py
+++ sportsdataverse/nhl/__init__.py
@@ -2,8 +2,11 @@ from __future__ import annotations
 
 from sportsdataverse.nhl.nhl_api import *
 from sportsdataverse.nhl.nhl_api_web import *
+from sportsdataverse.nhl.nhl_edge import *
+from sportsdataverse.nhl.nhl_stats_rest import *
 from sportsdataverse.nhl.nhl_game_rosters import *
 from sportsdataverse.nhl.nhl_loaders import *
 from sportsdataverse.nhl.nhl_pbp import *
+from sportsdataverse.nhl.nhl_records import *
 from sportsdataverse.nhl.nhl_schedule import *
 from sportsdataverse.nhl.nhl_teams import *
